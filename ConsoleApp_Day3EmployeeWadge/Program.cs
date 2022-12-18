b57f97b3b3e7d36e137ba75e20fe53511e3c2c4e
Divya <beingdivyadixit@gmail.com>
2022-12-18 14:25:38 +0530

    Updated to Employee is present or not by using Random

--- ConsoleApp_Day3EmployeeWadge/Program.cs
+++ ConsoleApp_Day3EmployeeWadge/Program.cs
@@ -7,6 +7,17 @@ namespace ConsoleApp_Day3EmployeeWadge
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Random random = new Random();
+            int empCheck1 = random.Next(0, 2);
+            int IS_FULL_TIME = 1;
+            if (empCheck1 == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is not Present");
+            }
         }
     }
 }
