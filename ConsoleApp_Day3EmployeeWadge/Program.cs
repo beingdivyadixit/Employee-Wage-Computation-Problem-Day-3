2dca7b34a911f494ea39a779379afae8db656500
Divya <beingdivyadixit@gmail.com>
2022-12-18 14:40:36 +0530

    Updated to Daily Employee Wadges Program

--- ConsoleApp_Day3EmployeeWadge/Program.cs
+++ ConsoleApp_Day3EmployeeWadge/Program.cs
@@ -9,15 +9,18 @@ namespace ConsoleApp_Day3EmployeeWadge
             Console.WriteLine("Welcome to Employee Wage Computation Program");
             Random random = new Random();
             int empCheck1 = random.Next(0, 2);
-            int IS_FULL_TIME = 1;
+            int IS_FULL_TIME = 1, EMP_RATE_PER_HOUR = 20;
+            int empWadge = 0, empHr = 0;
             if (empCheck1 == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHr = 8;
             }
             else
             {
-                Console.WriteLine("Employee is not Present");
+                empHr = 0;
             }
+            empWadge = empHr * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wadge:" + empWadge);
         }
     }
 }
