771fefce1b1d082f777b1f71175b8cc081ebc136
Divya <beingdivyadixit@gmail.com>
2022-12-26 15:45:56 +0530

    Refactor by creating method

--- ConsoleApp_Day3EmployeeWadge/Program.cs
+++ ConsoleApp_Day3EmployeeWadge/Program.cs
@@ -4,32 +4,44 @@ namespace ConsoleApp_Day3EmployeeWadge
 {
     internal class Program
     {
+       public const int EMP_RATE_PER_HOUR = 20, NUM_OF_WORKING_DAYS = 20, MAX_HRS_IN_MONTH = 100;
+
         static void Main(string[] args)
         {
+
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            int EMP_RATE_PER_HOUR = 20, NUM_OF_WORKING_DAYS = 20, MAX_HRS_IN_MONTH = 100;
-            int totalEmpHr = 0, empHr = 0, totalEmpWadge = 0, totalWorkingDays = 0;
-            while (totalEmpHr <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-            {
-                Random random = new Random();
-                int empCheck1 = random.Next(0, 3);
-                switch (empCheck1)
-                {
-                    case 1:
-                        empHr = 8;
-                        break;
-                    case 2:
-                        empHr = 4;
-                        break;
-                    default:
-                        empHr = 0;
-                        break;
-                }
-                totalEmpHr = totalEmpHr + empHr;
-                totalWorkingDays++;
-            }
-            totalEmpWadge = totalEmpHr * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Employee Wadge For A Month:" + totalEmpWadge);
+            calculateWages();
         }
+         
+        
+          public static void calculateWages()
+
+          {     
+                int totalEmpHr = 0, empHr = 0, totalEmpWadge = 0, totalWorkingDays = 0;
+                while (totalEmpHr <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+                {
+                    Random random = new Random();
+                    int empCheck1 = random.Next(0, 3);
+                    switch (empCheck1)
+                    {
+                        case 1:
+                            empHr = 8;
+                            break;
+                        case 2:
+                            empHr = 4;
+                            break;
+                        default:
+                            empHr = 0;
+                            break;
+                    }
+                    totalEmpHr = totalEmpHr + empHr;
+                    totalWorkingDays++;
+                }
+                totalEmpWadge = totalEmpHr * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Total Employee Wadge For A Month:" + totalEmpWadge);
+          }
+        
+
     }
+    
 }
