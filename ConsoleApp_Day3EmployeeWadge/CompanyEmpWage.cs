45b10896368fb5356d330efcc66c3d88efd9b085
Divya <beingdivyadixit@gmail.com>
2022-12-30 21:33:59 +0530

    Updated to Manage Employee Wages for Multiple Comapny program UC10

---
+++ ConsoleApp_Day3EmployeeWadge/CompanyEmpWage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Day3EmployeeWadge
+{
+    internal class CompanyEmpWage
+    {
+        public string company;
+        public int EMP_RATE_PER_HOUR;
+        public int NUM_OF_WORKING_DAYS;
+        public int MAX_HRS_IN_MONTH;
+        public int totalEmpWage;
+
+        public CompanyEmpWage(string company, int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH)
+        {
+            this.company = company;
+            this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
+            this.NUM_OF_WORKING_DAYS = NUM_OF_WORKING_DAYS;
+            this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
+        }
+
+        public void SetTotalWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+
+        public override string ToString()
+        {
+            return $"company:{company} RatePerHr:{EMP_RATE_PER_HOUR} Days:{NUM_OF_WORKING_DAYS} Hrs:{MAX_HRS_IN_MONTH}";
+        }
+    }
+}
--- ConsoleApp_Day3EmployeeWadge/EmpWageBuilderObject.cs
+++ ConsoleApp_Day3EmployeeWadge/EmpWageBuilderObject.cs
@@ -6,26 +6,45 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp_Day3EmployeeWadge
 {
-    public class EmpWageBuilderObject
+    class EmpWageBuilderObject
     {
-        private string company;
+      /*  private string company;
         private int EMP_RATE_PER_HOUR;
         private int NUM_OF_WORKING_DAYS;
         private int MAX_HRS_IN_MONTH;
-        private int totalEmpWage;
+        private int totalEmpWage;*/
+        CompanyEmpWage[] companies;
+        int numbOfCompanies = 0;
 
-        public EmpWageBuilderObject(string company, int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH) // creating constructor
+        public EmpWageBuilderObject() // creating constructor
         {
-            this.company = company;
-            this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
-            this.NUM_OF_WORKING_DAYS= NUM_OF_WORKING_DAYS;
-            this.MAX_HRS_IN_MONTH= MAX_HRS_IN_MONTH;
+            /* this.company = company;
+             this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
+             this.NUM_OF_WORKING_DAYS= NUM_OF_WORKING_DAYS;
+             this.MAX_HRS_IN_MONTH= MAX_HRS_IN_MONTH;*/
+            companies = new CompanyEmpWage[5];
         }
-        public  void computeEmpWages()   //Creating method for wages
+        public void AddComapnyDetailsIntoArray(string company, int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH)
+        {
+            CompanyEmpWage comp = new CompanyEmpWage(company, EMP_RATE_PER_HOUR, NUM_OF_WORKING_DAYS, MAX_HRS_IN_MONTH);
+            companies[numbOfCompanies] = comp;
+            numbOfCompanies++;
+        }
+
+        public void IterateOverCompany()
+        {
+            for (int i = 0; i < numbOfCompanies; i++)
+            {
+                int totalEmpWage = computeEmpWages(companies[i]);
+                companies[i].SetTotalWage(totalEmpWage);
+                Console.WriteLine(companies[i].ToString());
+            }
+        }
+        public  int computeEmpWages(CompanyEmpWage companyDetails)   //Creating method for wages
 
         {
             int totalEmpHr = 0, empHr = 0, totalEmpWadge = 0, totalWorkingDays = 0,empWage=0;
-            while (totalEmpHr <= this.MAX_HRS_IN_MONTH && totalWorkingDays < this.NUM_OF_WORKING_DAYS)  // while loop for condition
+            while (totalEmpHr <= companyDetails.MAX_HRS_IN_MONTH && totalWorkingDays < companyDetails.NUM_OF_WORKING_DAYS)  // while loop for condition
             {  
                 Random random = new Random();
                 int empCheck1 = random.Next(0, 3);
@@ -41,14 +60,14 @@ namespace ConsoleApp_Day3EmployeeWadge
                         empHr = 0;
                         break;
                 }
-                empWage = EMP_RATE_PER_HOUR * empHr;
+                empWage =companyDetails.EMP_RATE_PER_HOUR * empHr;
                 totalEmpHr = totalEmpHr + empHr;
-                totalEmpWage = totalEmpHr * EMP_RATE_PER_HOUR;
+                totalEmpWadge = totalEmpHr *companyDetails.EMP_RATE_PER_HOUR;
                 totalWorkingDays++;
             }
-            totalEmpWadge = totalEmpHr * this.EMP_RATE_PER_HOUR; // calculating the total value of employee wadge
-            Console.WriteLine("Total Employee wages for {0} {1} days:{2} and Hrs:{3}", company, (totalWorkingDays - 1), totalEmpWage, (totalEmpHr - empHr));
-
+            totalEmpWadge = totalEmpHr * companyDetails.EMP_RATE_PER_HOUR; // calculating the total value of employee wadge
+            //Console.WriteLine("Total Employee wages for {0} {1} days:{2} and Hrs:{3}", company, (totalWorkingDays - 1), totalEmpWage, (totalEmpHr - empHr));
+            return totalEmpWadge;
         }
 
       
